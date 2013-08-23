71a65070f1578558a118cf497c472295ac449430
Alejo Fernandez <alejo.fernandez@gmail.com>
2013-08-23 01:09:42 -0700

    Fifth lesson

--- mainController.js
+++ mainController.js
@@ -4,26 +4,34 @@
 // });
 
 angular.module('myFirstApp', [])
-  .controller('mainController', function ($scope) {
-    $scope.contacts = [{
-      name: 'Jose',
-      number: 1234
-    },
-    {
-      name: 'Carlos',
-      number: 333
-    },
-    {
-      name: 'Juan',
-      number: 555
-    }];
+  .controller('mainController', function ($scope, $http, $timeout) {
+    $scope.contacts = [];
+    // $scope.sortOrder = 'name';
+
+    $scope.sortBy = function (sortOrder) {
+      $scope.sortOrder = sortOrder;
+    };
+
+    $scope.loadContacts = function () {
+      $http({ method: 'GET', url: 'contacts.json' })
+        .success(function (contacts) {
+          $scope.contacts = contacts;
+        })
+        .error(function (error) {
+          console.log(error);
+        });
+    };
 
     $scope.addContact = function (name, number) {
-      $scope.contacts.push({ name: name, number: number })
+      $scope.contacts.push({ name: name, number: number });
+      $scope.name = '';
+      $scope.number = '';
     };
 
     $scope.deleteContact = function (contact) {
       var index = $scope.contacts.indexOf(contact);
       $scope.contacts.splice(index, 1);
     };
+
+    $scope.loadContacts();
   });
