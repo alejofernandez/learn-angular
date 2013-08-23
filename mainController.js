c598bd23145aeb603d6685812bbe176a957b064f
Alejo Fernandez <alejo.fernandez@gmail.com>
2013-08-22 23:19:56 -0700

    Third lesson

--- mainController.js
+++ mainController.js
@@ -5,5 +5,25 @@
 
 angular.module('myFirstApp', [])
   .controller('mainController', function ($scope) {
-    $scope.myValue = '';
+    $scope.contacts = [{
+      name: 'Jose',
+      number: 1234
+    },
+    {
+      name: 'Carlos',
+      number: 333
+    },
+    {
+      name: 'Juan',
+      number: 555
+    }];
+
+    $scope.addContact = function (name, number) {
+      $scope.contacts.push({ name: name, number: number })
+    };
+
+    $scope.deleteContact = function (contact) {
+      var index = $scope.contacts.indexOf(contact);
+      $scope.contacts.splice(index, 1);
+    };
   });
