bc5ad53ecb5171fe5635ea39df9bae1c2269a455
Alejo Fernandez <alejo.fernandez@gmail.com>
2013-08-22 22:46:02 -0700

    First lesson

---
+++ mainController.js
@@ -0,0 +1,9 @@
+// var app = angular.module('myFirstApp', []);
+// var mainController = app.controller('mainController', function ($scope) {
+
+// });
+
+angular.module('myFirstApp', [])
+  .controller('mainController', function ($scope) {
+    $scope.myValue = 'Hello World!!!';
+  });
